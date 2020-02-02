6bb94b0d519f45ef76c671f42d02c775c9d97a0a
Vlad <suzirie@gmail.com>
2020-02-02 21:52:39 +0200

    add debug area

--- index.js
+++ index.js
@@ -1,159 +1,288 @@
-var canvas1 = document.getElementById("myCanvasOne");
-var ctx1 = canvas1.getContext("2d");
-
-const canvas2 = document.getElementById("myCanvasTwo");
-const ctx2 = canvas2.getContext("2d");
-
-const canvas3 = document.getElementById("myCanvasThree");
-const ctx3 = canvas3.getContext("2d");
-
-const canvases = [ctx1, ctx2, ctx3];
-
-
-var x = 0;
-var y = 0;
-var dx = 0;
-var dy = 7;
-
-
-class Test {
-    name(params) {
-        console.log('6', params);
-    }
-}
-
-const a = new Test();
-a.name('----');
-
-const intervals = [];
-let start;
-
-const line = {
-    top: '',
-    middle: '',
-    bottom: ''
-};
-
-const win = [];
-
-const images = ['rhino','bunny','teddy'];
-var loadedImages = {};
-var promiseArray = images.map(function(imgName){
-   return new Promise(function(resolve,reject){
-       var img = new Image();
-       img.onload = function(){
-           loadedImages[imgName] = {};
-           loadedImages[imgName]['name'] = img;
-           loadedImages[imgName]['pos'] = 0;
-           resolve();
-       };
-       img.src = 'images/'+imgName+'.jpg';
-   });
-});
-
-const imagesInProcess = {
-    "0":[],
-    "1":[],
-    "2":[]
-};
-
-Promise.all(promiseArray).then(imagesLoaded);
-
-function imagesLoaded(){
-   //console.log(loadedImages);
-   init(0);
-   //init(1);
-   //init(2);
-   //setTimeout(loop, 2000);
-   //loop();
-   document.getElementById('start').addEventListener('click', () => {
-       start = Date.now();
-       /*
-       moveImage('rhino', 0);
-       moveImage('bunny', 100);
-       moveImage('teddy', 200);
-       */
-      loop();
-   });
-}
-
-function init(ind) {
-    const levelStep = 100;
-    let currentlevel = 0;
-    for (let imgName in loadedImages) {
-        if (loadedImages.hasOwnProperty(imgName)) {
-            console.log(canvases[ind]);
-            canvases[ind].drawImage(loadedImages[imgName]['name'], 20, currentlevel, 100, 100);
-            currentlevel += levelStep;
-            imagesInProcess[ind].push(imgName); 
-        }
-    }
-}
-
-function drawResults() {
-    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
-    for(let prop in loadedImages) {
-        if (loadedImages.hasOwnProperty(prop)) {
-            if (loadedImages[prop]['pos'] > 0 && loadedImages[prop]['pos'] < 149) {
-                ctx1.drawImage(loadedImages[prop]['name'], 20,0, 100, 100);
-            }
-            if (loadedImages[prop]['pos'] > 141 && loadedImages[prop]['pos'] < 250) {
-                ctx1.drawImage(loadedImages[prop]['name'], 20,100, 100, 100);
-            }
-            if (loadedImages[prop]['pos'] > 251 && loadedImages[prop]['pos'] < 350) {
-                ctx1.drawImage(loadedImages[prop]['name'], 20,200, 100, 100);
-            }
-        }
-    }
-}
-
-function loop() {
-    let st = 0;
-    for(let prop in imagesInProcess) {
-        imagesInProcess[prop].forEach(el => {
-            moveImage(el, st);
-            st += 100;
-        });
-    }
+class Slot {
     
-}
+    constructor(canvas, loadedImages, images){
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+        this.loadedImages = loadedImages;
+        this.images = images;
+        this.step = 13;
+        this.start = 0;
+        this.delta = 0;
+        this.intervals = [];
+        this.results = [];
+        this.debug = false;
 
-
-function moveImage(imageName, startY=0, startX) {
-
-    if ( (Date.now() - start)/1000 > 2 + Math.random(0,1) ) {
-        console.log(loadedImages);
-        intervals.forEach(int=>{
-            clearInterval(int);
-        });
-        drawResults();
-        return 77;
     }
 
-    const interval = setInterval(()=>{
-       startY += dy;
-       ctx1.clearRect(0, startY-dy, 120, startY+100+dy);
-       ctx1.drawImage(loadedImages[imageName]['name'], 20,startY, 100, 100);
-       loadedImages[imageName]['pos'] = startY;
-       //pos(imageName, startY);
-       intervals.push(interval);
-       if (startY > 330 ) {
-           clearInterval(interval);
-           startY = 0;
-           //const newImage = images[getRandomInt(images.length)];
-           moveImage(imageName, 0);
-           if (win.length === 3) {
-               win.length = 0;
+    init() {
+        const levelStep = 100;
+        let currentlevel = 0;
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (let imgName in this.loadedImages) {
+            if (this.loadedImages.hasOwnProperty(imgName)) {
+                this.ctx.drawImage(this.loadedImages[imgName]['name'], 20, currentlevel, 100, 100);
+                currentlevel += levelStep;
+            }
+        }
+    }
+
+    setDebugMode(mode) {
+        this.debug = mode;
+    }
+
+    setDelta(delta) {
+        this.delta = delta;
+    }
+
+    getResults() {
+        return this.results;
+    }
+
+    moveImage(imageName, startY=0) {
+        if ( (Date.now() - this.start)/1000 > 2 + this.delta +0.1) {
+            this.intervals.forEach( int => {
+                clearInterval(int);
+            });
+            this.intervals.length = 0;
+            this.drawResults();
+            return;
+        }
+    
+        const interval = setInterval(()=>{
+           startY += this.step;
+           this.ctx.clearRect(0, startY-this.step, 120, startY+100+this.step);
+           this.ctx.drawImage(this.loadedImages[imageName]['name'], 20,startY, 100, 100);
+           this.loadedImages[imageName]['pos'] = startY;
+           this.intervals.push(interval);
+           if (startY > 330 ) {
+               clearInterval(interval);
+               startY = 0;
+               this.moveImage(imageName, -200);
            }
-           win.push(imageName);
-       }
-    }, 10);
-}
+        }, 10);
+    }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-  
+    loop() {
+        if (!this.debug) this.results.length = 0;
+        
+        this.start = Date.now();
+        let st = -100;
+        for(let img of this.images) {
+            this.moveImage(img, st);
+            st += 100;
+        }
+    }
 
-let xx = 20;
-let yy = 20;
+    setResults(img1, img2, img3){
+        if (arguments.length === 0) this.results.length = 0;
+        this.results = [img1, img2, img3];
+    }
+
+    drawResults() {
+        console.log('res', this.results)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        if (this.results.length) {
+            this.ctx.drawImage(this.loadedImages[this.results[0]]['name'], 20,0, 100, 100);
+            this.ctx.drawImage(this.loadedImages[this.results[1]]['name'], 20,100, 100, 100);
+            this.ctx.drawImage(this.loadedImages[this.results[2]]['name'], 20,200, 100, 100);
+            return;
+        }
+        for(let prop in this.loadedImages) {
+            if (this.loadedImages.hasOwnProperty(prop)) {
+                if (this.loadedImages[prop]['pos'] >= 0 && this.loadedImages[prop]['pos'] < 149) {
+                    this.ctx.drawImage(this.loadedImages[prop]['name'], 20,0, 100, 100);
+                    this.results[0] = prop;
+                }
+                if (this.loadedImages[prop]['pos'] >= 150 && this.loadedImages[prop]['pos'] < 250) {
+                    this.ctx.drawImage(this.loadedImages[prop]['name'], 20,100, 100, 100);
+                    this.results[1] = prop;
+                }
+                if (this.loadedImages[prop]['pos'] >= 251 && this.loadedImages[prop]['pos'] < 350) {
+                    this.ctx.drawImage(this.loadedImages[prop]['name'], 20,200, 100, 100);
+                    this.results[2] = prop;
+                }
+            }
+        }
+        
+    }
+
+    fillBorders(pos) {
+        this.ctx.fillStyle = "#FF0000";
+        switch(pos) {
+            case 'top':
+                this.ctx.fillRect(0,0, 150, 5);
+                this.ctx.fillRect(0,100, 150, 5);
+                break;  
+        }
+    }
+
+}
+const images = ['2xBAR','3xBAR','BAR', '7', 'Cherry'];
+
+(async function(){
+    const loadedImages = {};
+    const promiseArray = images.map(function(imgName){
+        return new Promise(function(resolve,reject){
+            var img = new Image();
+            img.onload = function(){
+                loadedImages[imgName] = {};
+                loadedImages[imgName]['name'] = img;
+                loadedImages[imgName]['pos'] = 0;
+                resolve();
+            };
+            img.src = 'images/'+imgName+'.png';
+        });
+    });
+    await Promise.all(promiseArray).then(console.log('ok'));
+    const slot_1 = new Slot(document.getElementById("myCanvasOne"), loadedImages, images);
+    const slot_2 = new Slot(document.getElementById("myCanvasTwo"), loadedImages, images);
+    const slot_3 = new Slot(document.getElementById("myCanvasThree"), loadedImages, images);
+    slot_1.init();
+    slot_2.init();
+    slot_3.init();
+    
+
+    const resEl = document.getElementById('results');
+
+    document.getElementById('start').addEventListener('click', async() => {
+        resEl.innerHTML='';
+        let deltas = [Math.random(0, 1.5), Math.random(0, 1.5)+0.5, Math.random(0, 1.5)+1].sort();
+        slot_1.setDelta(deltas[0]);
+        slot_2.setDelta(deltas[1]);
+        slot_3.setDelta(deltas[2]);
+
+
+        const isDebug = document.getElementById('debug_area').style.display === 'block';
+
+        console.log('is: ', isDebug);
+
+        if (isDebug) {
+            slot_1.setDebugMode(true);
+            slot_2.setDebugMode(true);
+            slot_3.setDebugMode(true);
+            
+            const testValues = getTestValues();
+
+            slot_1.setResults(testValues[0][0], testValues[0][1], testValues[0][2]);
+            slot_2.setResults(testValues[1][0], testValues[1][1], testValues[1][2]);
+            slot_3.setResults(testValues[2][0], testValues[2][1], testValues[2][2]);
+        }
+
+        slot_1.loop();
+        slot_2.loop();
+        slot_3.loop();
+
+
+        setTimeout(() => {
+            const res1 = slot_1.getResults();
+            const res2 = slot_2.getResults();
+            const res3 = slot_3.getResults();
+            const res = checkCases(res1, res2, res3);
+            resEl.innerHTML= '<span>' + res + '</span>';
+            
+            slot_1.setDebugMode(false);
+            slot_2.setDebugMode(false);
+            slot_3.setDebugMode(false);
+
+
+        }, 4000)
+
+
+
+        function checkCases(res1, res2, res3) {
+            let resultScore = 0;
+
+            const bars = ['BAR', '2xBAR', '3xBAR']
+
+            const lines = [
+                [res1[0], res2[0], res3[0]],
+                [res1[1], res2[1], res3[1]],
+                [res1[2], res2[2], res3[2]],
+            ];
+
+            for(let i=0; i<lines.length; i++) {
+        
+                if (lines[i].every(el => el === 'Cherry')) {
+                    if (i===0) return 2000;
+                    if (i===1) return 1000;
+                    if (i===2) return 4000;
+                }
+                
+            }
+             
+            for(let i=0; i<lines.length; i++) {
+                if (lines[i].every(el => el === '7')) {
+                    return 150;
+                }
+            }    
+        
+            for(let i=0; i<lines.length; i++) {
+                if (lines[i].indexOf('7') != -1 && lines[i].indexOf('Cherry') != -1) {
+                    console.log(lines[i]);
+                    return 75;
+                }
+            }
+        
+            for(let i=0; i<lines.length; i++) {
+                if (lines[i].every(el => el === '3xBAR')) {
+                    return 50;
+                }
+            }
+                
+            for(let i=0; i<lines.length; i++) {
+                if (lines[i].every(el => el === '2xBAR')) {
+                    return 20;
+                }
+            }    
+        
+            for(let i=0; i<lines.length; i++) {
+                if (lines[i].every(el => el === 'BAR')) {
+                    return 10;
+                }
+            }   
+        
+            for(let i=0; i<lines.length; i++) {
+                let barSet = new Set();
+               lines[i].forEach( el => {
+                    if (bars.indexOf(el) != -1) {
+                        barSet.add(el);
+                    }
+                });
+        
+                if (barSet.size > 1) return 5;
+            }
+            
+            return 0
+
+        }
+
+        function getTestValues() {
+            const reel1 = [
+                document.getElementById('reel1_top').value,
+                document.getElementById('reel1_mid').value,
+                document.getElementById('reel1_bottom').value
+            ];
+            const reel2 = [
+                document.getElementById('reel2_top').value,
+                document.getElementById('reel2_mid').value,
+                document.getElementById('reel2_bottom').value
+            ];
+            const reel3 = [
+                document.getElementById('reel3_top').value,
+                document.getElementById('reel3_mid').value,
+                document.getElementById('reel3_bottom').value
+            ];
+
+            return [reel1, reel2, reel3];
+        }
+           
+    });
+
+})()
+
+
+
+
+
+
+
